285c6ea272c647dba15a28e0316ba3bd8713b0f0
Daniel <omnisurfer@gmail.com>
2017-04-01 12:21:38 -0700

    Initial Commit

---
+++ VS2012 Graphics Tests/MainForm.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace VS2012_Graphics_Tests
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.renderBackdrop = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.renderBackdrop)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // renderBackdrop
+            // 
+            this.renderBackdrop.BackColor = System.Drawing.Color.Black;
+            this.renderBackdrop.Location = new System.Drawing.Point(12, 12);
+            this.renderBackdrop.Name = "renderBackdrop";
+            this.renderBackdrop.Size = new System.Drawing.Size(260, 207);
+            this.renderBackdrop.TabIndex = 0;
+            this.renderBackdrop.TabStop = false;
+            this.renderBackdrop.Paint += new System.Windows.Forms.PaintEventHandler(this.renderBackdrop_Paint);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 262);
+            this.Controls.Add(this.renderBackdrop);
+            this.MinimumSize = new System.Drawing.Size(500, 300);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.renderBackdrop)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox renderBackdrop;
+    }
+}
+
---
+++ VS2012 Graphics Tests/MainForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VS2012_Graphics_Tests
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            UpdateBackdrop(e);
+        }
+
+        private void DrawSin(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            Pen pen = new Pen(Color.Green, 2);
+
+            PointF[] points = new PointF[renderBackdrop.Right];
+
+            float xOffset = 0.0F, yOffset = renderBackdrop.Bottom / 2, xScale = 1.0F, yScale = renderBackdrop.Bottom;
+
+            float freq = 0.01F, phase = 0.0F, amplitude = 0.25F;
+
+            /*
+             * For a real application, it would be best to calculate the sine wave somewhere else and then take the resulting array and map it to the paint surface.
+             * Probably would need to calculate at least twice as many points for pixels in display (like nyquist sampling theory).
+             */
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i] = new PointF(
+                    (i + xOffset),
+                    ((yScale * amplitude * (float)Math.Sin(i * freq * Math.PI + phase)) + yOffset)
+                    );
+            }
+
+            g.DrawLines(pen, points);
+        }
+
+        private void DrawCenterLine(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            Pen pen = new Pen(Color.Yellow, 1);
+
+            g.DrawLine(pen, 0.0F, renderBackdrop.Bottom / 2, renderBackdrop.Right, renderBackdrop.Bottom / 2);
+        }
+
+        private void UpdateBackdrop(EventArgs e)
+        {
+            renderBackdrop.Height = Convert.ToInt32(0.8F * ClientRectangle.Height);
+            renderBackdrop.Width = Convert.ToInt32(0.8F * ClientRectangle.Width);
+        }
+
+        private void renderBackdrop_Paint(object sender, PaintEventArgs e)
+        {
+            DrawCenterLine(e);
+
+            DrawSin(e);
+
+            System.Diagnostics.Debug.WriteLine("renderBackdrop Paint Event Triggered!");
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            UpdateBackdrop(e);
+
+            renderBackdrop.Invalidate();
+        }
+    }
+}
